f8875abb52f3e39728dc19f6723d436c57637e65
Terungwa_Ayerhan <terayerhan@outlook.com>
2024-09-05 12:12:42 +1000

    Implement Format::ElsapsedTime()

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,13 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long uptime_seconds) { 
+    long int hours = uptime_seconds / 3600;
+    uptime_seconds %= 3600;
+    long int minutes = uptime_seconds / 60;
+    long int seconds = uptime_seconds % 60;
+
+    return std::to_string(hours) + ":" +
+           std::to_string(minutes) + ":" +
+           std::to_string(seconds); 
+}
