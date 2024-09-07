fa4dc6c7c4a8cd0c70449f4ad4f33cac4dd7a9ba
Terungwa_Ayerhan <terayerhan@outlook.com>
2024-09-07 18:22:56 +1000

    Implement Processor::Utilization()

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,19 @@
 #include "processor.h"
+#include "linux_parser.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+    long currentTotalJiffies = LinuxParser::Jiffies();
+    long currentActiveJiffies = LinuxParser::ActiveJiffies();
+
+    long totalDiff = currentTotalJiffies - previousTotalJiffies_;
+    long activeDiff = currentActiveJiffies - previousActiveJiffies_;
+
+    if(totalDiff == 0){ return 0.0;}
+
+    // Cache the current values in the Processor object.
+    previousTotalJiffies_ = currentTotalJiffies;
+    previousActiveJiffies_ = currentActiveJiffies;
+
+    return static_cast<float>(activeDiff) / totalDiff; 
+}
