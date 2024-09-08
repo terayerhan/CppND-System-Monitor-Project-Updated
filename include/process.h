be15709d7b7edc667cc97d59fa6172e434f3b3cb
Terungwa_Ayerhan <terayerhan@outlook.com>
2024-09-09 09:58:20 +1000

    Change Process() to use new attributes and methods

--- include/process.h
+++ include/process.h
@@ -8,7 +8,9 @@ It contains relevant attributes as shown below
 */
 class Process {
  public:
-  Process(int pid) : pid_(pid) {}  // Process constructor
+  Process(int pid) : pid_(pid), valid_(true), lastTotalTime_(0), lastTotalSystemTime_(0), cpuUtilization_(0.0) {
+        updateInfo();
+  }
   int Pid();                               // TODO: See src/process.cpp
   std::string User();                      // TODO: See src/process.cpp
   std::string Command();                   // TODO: See src/process.cpp
@@ -26,6 +28,9 @@ class Process {
   unsigned long long lastTotalTime_;
   unsigned long long lastTotalSystemTime_;
   unsigned long long startTime_;
+
+  void updateInfo();
+  bool hasChanged();
 };
 
 #endif
