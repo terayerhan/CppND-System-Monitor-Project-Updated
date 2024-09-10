74f470753ae6e8c59ee4a770249f8b7d2d783d06
Terungwa_Ayerhan <terayerhan@outlook.com>
2024-09-10 11:51:15 +1000

    Change the comparision operator to > (greater)

--- include/process.h
+++ include/process.h
@@ -18,7 +18,7 @@ class Process {
   float CpuUtilization();                  // TODO: See src/process.cpp
   std::string Ram();                       // TODO: See src/process.cpp
   long int UpTime();                       // TODO: See src/process.cpp
-  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  bool operator>(Process const& a) const;  // TODO: See src/process.cpp
 
   // TODO: Declare any necessary private members
  private:
